c087a11845c02cb6ddc25cfc7b790ef6cd39a450
Khoa Nguyen <khoa@clout.com>
2017-08-26 15:28:15 -0700

    [feat] added error message for duplicate uuid

--- app/controllers/ImpressionController.js
+++ app/controllers/ImpressionController.js
@@ -2,26 +2,49 @@ const models = require('../config/db.config.js');
 
 module.exports = {
 
+  /**
+   * Get the number of impressions
+   */
   getAllimpressions: function(req, res, next) {
-    models.Impression.findAll({})
+    models.Impression.count()
     .then((data) => {
-      res.json(data.length);
+      res.json(data);
     });
   },
 
+  /**
+   * Will add an impression to the database
+   * returns 200 if successful
+   * returns 400 if uuid and timestamp were not passed in
+   */
   addOneimpression: function(req, res, next) {
-    var uniqueId = Math.random().toString(36).substring(2) 
-    + (new Date()).getTime().toString(36);
-    res.json(req.query)
-    // models.Impression.create({
-    //   uuid: req.query.uuid,
-    //   timestamp: req.query.timestamp
-    // })
-    //  .then((user) => {
-    //    res.json(user);
-    //  })
-    //  .catch((err) => {
-    //    res.json(err);
-    //  });
+    if(req.query.uuid && req.query.timestamp){
+      models.Impression.findOne({
+        where: {
+          uuid: req.query.uuid
+        }
+      })
+      .then(data => {
+        if(data) res.status(400).json("Duplicate uuid detected, please try again")
+        else{
+          models.Impression.create({
+            uuid: req.query.uuid,
+            timestamp: req.query.timestamp
+          })
+           .then((user) => {
+             res.json(user);
+           })
+           .catch((err) => {
+             res.json(err);
+           });
+        }
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+    }else{
+      res.status(400).json("Please enter a valid uuid and timestamp")
+    }
+    
   }
 }
